94262c817ae974e8c330910268417c76c3a7820c
Yuxuan Cai <1093369126@qq.com>
2024-09-24 18:51:33 -0400

    Interview page update

--- src/components/CollaborativeEditor.js
+++ src/components/CollaborativeEditor.js
@@ -1,9 +1,9 @@
-// CollaborativeEditor.jsx
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 import Peer from 'simple-peer';
-import CodeEditor from './CodeEditor'; // Adjust the path as necessary
+import CodeEditor from './CodeEditor';
+import './CollaborativeEditor.css';
 
 const CollaborativeEditor = () => {
   const { roomId } = useParams();
@@ -45,17 +45,17 @@ const CollaborativeEditor = () => {
 
     // Get user media
     navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        setStream(stream);
-        if (myVideo.current) {
-          myVideo.current.srcObject = stream;
-        }
-        console.log('Obtained user media stream');
-      })
-      .catch((err) => {
-        console.error('Failed to get media stream:', err);
-      });
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          setStream(stream);
+          if (myVideo.current) {
+            myVideo.current.srcObject = stream;
+          }
+          console.log('Obtained user media stream');
+        })
+        .catch((err) => {
+          console.error('Failed to get media stream:', err);
+        });
 
     // Handle incoming calls
     socketRef.current.on('callUser', (data) => {
@@ -211,7 +211,7 @@ const CollaborativeEditor = () => {
   // Handle running code
   const handleRunCode = async () => {
     try {
-      const response = await fetch('http://localhost:8080/code/code/execute', { // Update with your backend URL
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/code/code/execute`, { // Update with your backend URL
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -229,107 +229,107 @@ const CollaborativeEditor = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      {/* Video Call and Controls */}
-      <div style={{ width: '50%', padding: '20px' }}>
-        {/* Video Streams */}
-        <div>
-          {stream && (
-            <video
-              playsInline
-              muted
-              ref={myVideo}
-              autoPlay
-              style={{ width: '300px', marginRight: '10px' }}
-            />
-          )}
-          {callAccepted && !callEnded && (
-            <video
-              playsInline
-              ref={userVideo}
-              autoPlay
-              style={{ width: '300px' }}
-            />
-          )}
-        </div>
+      <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        {/* Video Call and Controls */}
+        <div style={{ width: '50%', padding: '20px' }}>
+          {/* Video Streams */}
+          <div>
+            {stream && (
+                <video
+                    playsInline
+                    muted
+                    ref={myVideo}
+                    autoPlay
+                    style={{ width: '300px', marginRight: '10px' }}
+                    className={"rounded-2xl"}
+                />
+            )}
+            {callAccepted && !callEnded && (
+                <video
+                    playsInline
+                    ref={userVideo}
+                    autoPlay
+                    style={{ width: '300px' }}
+                    className={"rounded-2xl"}
+                />
+            )}
+          </div>
 
-        {/* Incoming Call Notification */}
-        <div>
-          {receivingCall && !callAccepted && (
-              <div className="modal">
-                <div className="modal-content">
-                  <h2 className="text-2xl mb-4">{name}is calling...</h2>
+          {/* Incoming Call Notification */}
+          <div>
+            {receivingCall && !callAccepted && (
+                <div className="modal">
+                  <div className="modal-content">
+                    <h2 className="text-2xl mb-4">You have a video call...</h2>
 
-                  <div className="">
-                    <button
-                        onClick={answerCall}
-                        className=" bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
-                    >
-                      Answer
-                    </button>
+                    <div className="">
+                      <button
+                          onClick={answerCall}
+                          className=" bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
+                      >
+                        Answer
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-          )
-          //     (
-          //   <div>
-          //     <h1>{name} is calling...</h1>
-          //     <button onClick={answerCall}>Answer</button>
-          //   </div>
-          // )
+            )}
+          </div>
 
-          }
+          {/* Participants and Call Controls */}
+          <div>
+            {/*<h2>Participants:</h2>*/}
+            {usersInRoom.map((userId) => (
+                <div key={userId} style={{ marginBottom: '5px', marginTop: '5px'}}>
+                  <span>participant</span>
+                  <button
+                      onClick={() => callUser(userId)}
+                      style={{ marginLeft: '10px' }}
+                      className="bg-green-500 hover:bg-green-600 text-white font-semibold px-2 rounded-3xl"
+                  >
+                    Call
+                  </button>
+                  {callAccepted && !callEnded && (
+                      <button
+                          onClick={leaveCall}
+                          style={{ marginTop: '10px', marginLeft: '20px'}}
+                          className="bg-red-500 hover:bg-red-600 text-white font-semibold px-2 rounded-3xl"
+                      >
+                        End Call
+                      </button>
+                  )}
+                </div>
+            ))}
+
+          </div>
         </div>
 
-        {/* Participants and Call Controls */}
-        <div>
-          <h2>Participants</h2>
-          {usersInRoom.map((userId) => (
-            <div key={userId} style={{ marginBottom: '5px' }}>
-              <span>{userId}</span>
-              <button
-                onClick={() => callUser(userId)}
-                style={{ marginLeft: '10px' }}
-                className="bg-green-500 hover:bg-green-600 text-white font-semibold px-2 rounded-3xl"
-
-              >
-                Call
-              </button>
-            </div>
-          ))}
-          {callAccepted && !callEnded && (
+        {/* Code Editor Section */}
+        <div style={{width: '50%', height: '70vh'}}>
+          <CodeEditor
+              code={code}
+              onChange={handleCodeChange}
+              language={language}
+              setLanguage={handleLanguageChange}
+          />
+          {/* Run Button */}
+          <div className="run-output-container">
             <button
-                onClick={leaveCall}
-                style={{ marginTop: '10px' }}
-                className="bg-red-500 hover:bg-red-600 text-white font-semibold px-2 rounded-3xl"
-
+                onClick={handleRunCode}
+                className="run-button"
             >
-              End Call
+              Run
             </button>
-          )}
+
+            {/* Output Display */}
+            <div className="output-container">
+              <h3>Output:</h3>
+              <pre className="output-pre">
+                {output}
+              </pre>
+            </div>
+          </div>
         </div>
       </div>
-
-      {/* Code Editor Section */}
-      <div style={{ width: '50%', padding: '20px' }}>
-        <CodeEditor
-          code={code}
-          onChange={handleCodeChange}
-          language={language}
-          setLanguage={handleLanguageChange}
-        />
-        {/* Run Button */}
-        <button onClick={handleRunCode} style={{ marginTop: '10px' }}>
-          Run
-        </button>
-        {/* Output Display */}
-        <div style={{ marginTop: '20px' }}>
-          <h3>Output:</h3>
-          <pre>{output}</pre>
-        </div>
-
-      </div>
-    </div>
   );
 };
 
